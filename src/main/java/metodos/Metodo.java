66fdda4f206c3b1dd205be82551a5e7f1cb06e69
JanierGomez <janiergomez211@gmail.com>
2022-02-04 17:23:08 -0500

    Realice un cambio

--- src/main/java/metodos/Metodo.java
+++ src/main/java/metodos/Metodo.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package metodos;
 
-/**
- *
- * @author Janier Gómez
- */
+
 public class Metodo {
+    private int m;
     
 }
